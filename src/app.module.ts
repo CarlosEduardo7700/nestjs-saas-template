2c781fb7c025c07e3ee8a12bacab695bbff2862d
CarlosEduardo0077 <mendoncaeduardo652@gmail.com>
2026-02-18 22:40:26 -0300

    feat: Implement User module with CRUD operations and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseConfigService } from './database/config/database.config.service';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRootAsync({
       useClass: DatabaseConfigService,
     }),
+    UserModule,
   ],
   controllers: [],
   providers: [],
