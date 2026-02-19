9edfe38ec3f94df056965f6d8f1e73de35bdf90c
CarlosEduardo0077 <mendoncaeduardo652@gmail.com>
2026-02-18 22:28:42 -0300

    feat: Implement BaseEntity class with TypeORM decorators for common entity fields

---
+++ src/shared/base/BaseEntity.ts
@@ -0,0 +1,32 @@
+import {
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+export abstract class BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    nullable: false,
+    type: 'timestamp with time zone',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    nullable: false,
+    type: 'timestamp with time zone',
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    nullable: true,
+    type: 'timestamp with time zone',
+  })
+  deletedAt: Date;
+}
