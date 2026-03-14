548e78e1f693ed5d1eda5dec4927bdac65d8449c
CarlosEduardo0077 <mendoncaeduardo652@gmail.com>
2026-03-13 21:46:04 -0300

    feat: Generate the JWT access token

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { ControllerResponseDto } from 'src/shared/base/dtos/response/controller-respose.dto';
+import { LoginResponseDto } from './dto/login-response.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,7 +10,7 @@ export class AuthController {
 
   @Post('/login')
   async login(@Body() loginDto: LoginDto): Promise<ControllerResponseDto> {
-    const token: string = await this.authService.login(loginDto);
+    const token: LoginResponseDto = await this.authService.login(loginDto);
 
     return {
       message: 'Login successful!',
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,12 +3,18 @@ import { LoginDto } from './dto/login.dto';
 import { UserService } from '../user/user.service';
 import { User } from '../user/user.entity';
 import bcrypt from 'node_modules/bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { LoginResponseDto } from './dto/login-response.dto';
+import { JwtPayload } from './interface/jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  async login(loginDto: LoginDto): Promise<string> {
+  async login(loginDto: LoginDto): Promise<LoginResponseDto> {
     const user: User | null = await this.userService.getUserByEmailForAuth(
       loginDto.email,
     );
@@ -16,6 +22,10 @@ export class AuthService {
     if (!user || !(await bcrypt.compare(loginDto.password, user.password)))
       throw new UnauthorizedException('Invalid email or password!');
 
-    return 'fake-jwt-token';
+    const payload: JwtPayload = { sub: user.id, email: user.email };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
